9e13ea84ea504d882252c24c873b5880ac3854d3
Ke Song <33835813+Ke1Song@users.noreply.github.com>
2023-04-13 20:03:39 +0800

    Add files via upload

---
+++ DADD/ImageNet-Subset/main.py
@@ -0,0 +1,147 @@
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import StepLR
+from torch.autograd import Variable
+import os
+import math
+import argparse
+# import resource
+import numpy as np
+import sklearn.metrics
+from ResNet_imagenet import resnet18_ImageNet
+from DADD import DADD
+from data_manager_imagenet import *
+import torch
+
+parser = argparse.ArgumentParser(description='Class Incremental Learning via Dual Augmentation and Dual Augmentation')
+parser.add_argument('--epochs', default=141, type=int, help='Total number of epochs to run')
+parser.add_argument('--batch_size', default=64, type=int, help='Batch size for training')
+parser.add_argument('--print_freq', default=10, type=int, help='print frequency (default: 10)')
+parser.add_argument('--data_name', default='ImageNet100', type=str, help='Dataset name to use')
+parser.add_argument('--total_nc', default=100, type=int, help='class number for the dataset')
+parser.add_argument('--fg_nc', default=50, type=int, help='the number of classes in first task')
+parser.add_argument('--task_num', default=5, type=int, help='the number of incremental steps')
+parser.add_argument('--learning_rate', default=0.001, type=float, help='initial learning rate')
+parser.add_argument('--kd_weight', default=10.0, type=float, help='knowledge distillation loss weight')
+parser.add_argument('--temp', default=0.1, type=float, help='trianing time temperature')
+parser.add_argument('--gpu', default='0', type=str, help='GPU id to use')
+parser.add_argument('--save_path', default='model_check_DADD/', type=str, help='save files directory')
+args = parser.parse_args()
+
+
+def main():
+    data_manager = DataManager()
+    cuda_index = "cuda:" + args.gpu
+    device = torch.device(cuda_index if torch.cuda.is_available() else "cpu")
+    print(device)
+    task_size = int((args.total_nc - args.fg_nc) / args.task_num)  # number of classes in each incremental step
+    file_name = args.data_name + '_' + str(args.fg_nc) + '_' + str(args.task_num) + 'x' + str(task_size)
+    feature_extractor = resnet18_ImageNet()
+    model = DADD(args, file_name, feature_extractor, task_size, device)
+    model.checkpoint()
+    class_set = list(range(args.total_nc))
+
+    for i in range(1, args.task_num + 1):
+        print('task %d' % i)
+        if i == 0:
+            old_class = 0
+        else:
+            old_class = len(class_set[:args.fg_nc + (i - 1) * task_size])
+        model.beforeTrain(i)
+        model.train(i, old_class=old_class)
+        model.afterTrain()
+
+    ####### Test ######
+    test_transform = transforms.Compose([
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    print("############# Test for each Task #############")
+    acc_all = []
+    for current_task in range(args.task_num + 1):
+        class_index = args.fg_nc + current_task * task_size
+        filename = args.save_path + file_name + '/' + '%d_model.pkl' % class_index
+        model = torch.load(filename, map_location=cuda_index)
+        model.eval()
+        acc_up2now = []
+        for i in range(current_task + 1):
+            if i == 0:
+                classes = class_set[:args.fg_nc]
+            else:
+                classes = class_set[(args.fg_nc + (i - 1) * task_size):(args.fg_nc + i * task_size)]
+            testfolder = data_manager.get_dataset(test_transform, index=classes, train=False)
+            test_loader = torch.utils.data.DataLoader(
+                testfolder, batch_size=100,
+                shuffle=False,
+                drop_last=True, num_workers=4)
+            correct, total = 0.0, 0.0
+            for setp, data in enumerate(test_loader):
+                imgs, labels = data
+                imgs, labels = imgs.cuda(), labels.cuda()
+                with torch.no_grad():
+                    outputs = model(imgs)
+                predicts = torch.max(outputs, dim=1)[1]
+                correct += (predicts.cpu() == labels.cpu()).sum()
+                total += len(labels)
+            accuracy = correct.item() / total
+            acc_up2now.append(accuracy)
+        if current_task < args.task_num:
+            acc_up2now.extend((args.task_num - current_task) * [0])
+        acc_all.append(acc_up2now)
+        print(acc_up2now)
+    # print(acc_all)
+    a = np.array(acc_all)
+    result = []
+    for i in range(args.task_num + 1):
+        if i == 0:
+            result.append(0)
+        else:
+            res = 0
+            for j in range(i + 1):
+                res += (np.max(a[:, j]) - a[i][j])
+            res = res / i
+            result.append(100 * res)
+    print(50 * '#')
+    print('Forgetting result:')
+    print(result)
+    print("############# Test for up2now Tasks #############")
+    average_acc = 0
+    for current_task in range(args.task_num + 1):
+        class_index = args.fg_nc + current_task * task_size
+        filename = args.save_path + file_name + '/' + '%d_model.pkl' % class_index
+        model = torch.load(filename, map_location=cuda_index)
+        model.to(device)
+        model.eval()
+
+        classes = class_set[:(args.fg_nc + current_task * task_size)]
+        testfolder = data_manager.get_dataset(test_transform, index=classes, train=False)
+        test_loader = torch.utils.data.DataLoader(
+            testfolder, batch_size=100,
+            shuffle=False,
+            drop_last=False, num_workers=4)
+        correct, total = 0.0, 0.0
+        for setp, data in enumerate(test_loader):
+            imgs, labels = data
+            imgs, labels = imgs.cuda(), labels.cuda()
+            with torch.no_grad():
+                outputs = model(imgs)
+            predicts = torch.max(outputs, dim=1)[1]
+            correct += (predicts.cpu() == labels.cpu()).sum()
+            total += len(labels)
+        accuracy = correct.item() / total
+        print(accuracy)
+        average_acc += accuracy
+    print('average acc: ')
+    print(average_acc / (args.task_num + 1))
+
+
+if __name__ == "__main__":
+    main()
---
+++ DADD/cifar/DADD.py
@@ -0,0 +1,430 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+from torchvision import transforms
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import StepLR
+from torch.autograd import Variable
+import cv2
+import os
+import sys
+import numpy as np
+from myNetwork_classaug import network
+from iCIFAR100 import iCIFAR100
+from torchvision.transforms import autoaugment
+import torchvision
+from torchvision import models
+import time
+
+
+class DADD:
+    def __init__(self, args, file_name, feature_extractor, task_size, device):
+        self.args = args
+        self.numclass = self.args.fg_nc
+        self.task_size = task_size
+        self.file_name = file_name
+        self.radius = 0
+        self.augnumclass = 4 * self.numclass + int(self.numclass * (self.numclass - 1) / 2)
+        self.model = network(self.augnumclass, feature_extractor)
+        self.cov = None
+        self.prototype = None
+        self.class_label = None
+        self.old_model = None
+        self.gradcam_layer = self.model.feature.layer4
+        self.device = device
+        self.train_transform = transforms.Compose([transforms.RandomCrop((32, 32), padding=4),
+                                                   transforms.RandomHorizontalFlip(p=0.5),
+                                                   transforms.ColorJitter(brightness=0.24705882352941178),
+                                                   transforms.ToTensor(),
+                                                   transforms.Normalize((0.5071, 0.4867, 0.4408),
+                                                                        (0.2675, 0.2565, 0.2761))])
+        self.test_transform = transforms.Compose([transforms.ToTensor(),
+                                                  transforms.Normalize((0.5071, 0.4867, 0.4408),
+                                                                       (0.2675, 0.2565, 0.2761))])
+
+        self.train_dataset = iCIFAR100('./dataset', transform=self.train_transform, download=True)
+        self.test_dataset = iCIFAR100('./dataset', test_transform=self.test_transform, train=False, download=True)
+        self.train_loader = None
+        self.test_loader = None
+
+    def beforeTrain(self, current_task):
+        self.model.eval()
+        if current_task == 0:
+            classes = [0, self.numclass]
+        else:
+            classes = [self.numclass - self.task_size,
+                       self.numclass]
+        self.train_loader, self.test_loader = self._get_train_and_test_dataloader(classes)
+        if current_task > 0:
+            old_class = self.numclass - self.task_size
+            self.augnumclass = 4 * self.numclass + int(self.task_size * (self.task_size - 1) / 2)
+            self.model.Incremental_learning(old_class, self.augnumclass)
+        # print(self.model.fc.weight.shape)
+        self.model.train()
+        self.model.to(self.device)
+
+    def _get_train_and_test_dataloader(self, classes):
+        self.train_dataset.getTrainData(classes)
+        self.test_dataset.getTestData(classes)
+        train_loader = DataLoader(dataset=self.train_dataset,
+                                  shuffle=True,
+                                  batch_size=self.args.batch_size, num_workers=8)
+
+        test_loader = DataLoader(dataset=self.test_dataset,
+                                 shuffle=True,
+                                 batch_size=self.args.batch_size, num_workers=8)
+        return train_loader, test_loader
+
+    def train(self, current_task, old_class=0):
+        opt = torch.optim.Adam(self.model.parameters(), lr=self.args.learning_rate, weight_decay=2e-4)
+        scheduler = StepLR(opt, step_size=45, gamma=0.1)
+        accuracy = 0
+        for epoch in range(self.args.epochs):
+            for step, (indexs, images, target) in enumerate(self.train_loader):
+                opt.zero_grad()
+                loss = self._compute_loss(images, target, old_class)
+                opt.zero_grad()
+                loss.backward()
+                opt.step()
+            scheduler.step()
+            if epoch % self.args.print_freq == 0:
+                accuracy = self._test(self.test_loader)
+                print('epoch:%d,accuracy:%.5f' % (epoch, accuracy))
+        self.protoSave(self.model, self.train_loader, current_task)
+        return accuracy
+
+    def _test(self, testloader):
+        self.model.eval()
+        correct, total = 0.0, 0.0
+        for setp, (indexs, imgs, labels) in enumerate(testloader):
+            imgs, labels = imgs.to(self.device), labels.to(self.device)
+            with torch.no_grad():
+                outputs = self.model(imgs)
+                outputs = outputs[:, :self.numclass]
+            predicts = torch.max(outputs, dim=1)[1]
+            correct += (predicts.cpu() == labels.cpu()).sum()
+            total += len(labels)
+        accuracy = correct.item() / total
+        self.model.train()
+        return accuracy
+
+    def attention_distillation_loss(self, attention_map1, attention_map2):
+        """Calculates the attention distillation loss"""
+        attention_map1 = torch.norm(attention_map1, p=2, dim=0)
+        attention_map2 = torch.norm(attention_map2, p=2, dim=0)
+        return torch.norm(attention_map2 - attention_map1, p=1, dim=1).sum().mean()
+
+    def _compute_loss(self, imgs, target, old_class=0):
+        imgs, target = imgs.to(self.device), target.to(self.device)
+        imgs, target = self.classAug(imgs, target)
+        output = self.model(imgs)
+        loss_cls = nn.CrossEntropyLoss()(output / self.args.temp, target)
+        if self.old_model is None:
+            return loss_cls
+        else:
+            with GradCAM(self.model, self.model.feature.layer4) as gradcam:
+                with GradCAM(self.old_model, self.old_model.feature.layer4) as gradcam_old:
+                    attmap_old = gradcam_old(imgs)
+                    attmap = gradcam(imgs)
+                    attmap = torch.from_numpy(attmap)
+                    attmap_old = torch.from_numpy(attmap_old)
+                    attention_loss = self.attention_distillation_loss(attmap_old, attmap)
+                    feature = self.model.feature(imgs)
+                    feature_old = self.old_model.feature(imgs)
+                    loss_kd = torch.dist(feature, feature_old, 2)
+                    proto_aug = []
+                    proto_aug_label = []
+                    index = list(range(old_class))
+                    for _ in range(self.args.batch_size):
+                        np.random.shuffle(index)
+                        temp = self.prototype[index[0]] + random.uniform(0.0, self.radius) * np.random.normal(0, 1, 512)
+                        proto_aug.append(temp)
+                        proto_aug_label.append(self.class_label[index[0]])
+
+                    proto_aug = torch.from_numpy(np.float32(np.asarray(proto_aug))).float().to(self.device)
+                    proto_aug_label = torch.from_numpy(np.asarray(proto_aug_label)).to(self.device).long()
+                    soft_feat_aug = self.model.fc(proto_aug)
+                    soft_feat_aug = soft_feat_aug[:, :self.numclass]
+                    ratio = 2.5
+                    isda_aug_proto_aug = self.semanAug(proto_aug, soft_feat_aug, proto_aug_label, ratio)
+                    loss_semanAug = nn.CrossEntropyLoss()(isda_aug_proto_aug / self.args.temp, proto_aug_label)
+                    return loss_cls + self.args.seman_weight * loss_semanAug + self.args.kd_weight * loss_kd + \
+                           0.4 * attention_loss
+
+    def afterTrain(self):
+        path = self.args.save_path + self.file_name + '/'
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        filename = path + '%d_model.pkl' % self.numclass
+        self.model.saveOption(self.numclass)
+        torch.save(self.model, filename)
+        self.old_model = torch.load(filename)
+        self.old_model.to(self.device)
+        self.old_model.eval()
+        self.numclass += self.task_size
+
+    def protoSave(self, model, loader, current_task):  # self.protoSave(self.model, self.train_loader, current_task)
+        features = []
+        labels = []
+        model.eval()
+        with torch.no_grad():  # images.shape=64x3x32x32
+            for i, (indexs, images, target) in enumerate(loader):
+                feature = model.feature(images.to(self.device))
+                if feature.shape[0] == self.args.batch_size:  # feature.shape=64x512
+                    labels.append(target.numpy())  # target.size=64         #feature[0].shape=512 feature.shape[0]=64
+                    features.append(feature.cpu().numpy())
+        labels_set = np.unique(labels)
+        labels = np.array(labels)  # before label reshape (390, 64)
+        labels = np.reshape(labels, labels.shape[0] * labels.shape[1])  # after label  reshape (24960,)-24960=(390*64)
+        features = np.array(features)  # before feature reshape 390x64x512
+        features = np.reshape(features, (features.shape[0] * features.shape[1], features.shape[2]))
+        feature_dim = features.shape[1]
+        # after feature reshape (24960, 512)
+        prototype = []
+        cov = []
+        radius = []
+        class_label = []
+        for item in labels_set:
+            index = np.where(item == labels)[0]
+            class_label.append(item)
+            feature_classwise = features[index]
+            prototype.append(np.mean(feature_classwise, axis=0))
+            cov_class = np.cov(feature_classwise.T)
+            cov.append(cov_class)
+            if current_task == 0:
+                cov1 = np.cov(feature_classwise.T)
+                radius.append(np.trace(cov1) / feature_dim)
+        if current_task == 0:
+            self.radius = np.sqrt(np.mean(radius))
+            self.cov = np.concatenate(cov, axis=0).reshape([-1, 512, 512])
+            self.prototype = prototype
+            self.class_label = class_label
+            print(self.radius)
+        else:
+            self.cov = np.concatenate((cov, self.cov), axis=0)
+            self.prototype = np.concatenate((prototype, self.prototype), axis=0)
+            self.class_label = np.concatenate((class_label, self.class_label), axis=0)
+
+    def semanAug(self, features, y, labels, ratio):
+        N = features.size(0)
+        C = self.numclass
+        A = features.size(1)
+        weight_m = list(self.model.fc.parameters())[0]  # 512
+        weight_m = weight_m[:self.numclass, :]
+        NxW_ij = weight_m.expand(N, C, A)
+        NxW_kj = torch.gather(NxW_ij, 1, labels.view(N, 1, 1).expand(N, C, A))
+        labels = labels.cpu()
+        CV = self.cov
+        CV_temp = torch.from_numpy(CV[labels]).to(self.device)
+        sigma2 = ratio * torch.bmm(torch.bmm(NxW_ij - NxW_kj, CV_temp.float()),
+                                   (NxW_ij - NxW_kj).permute(0, 2, 1))
+        sigma2 = sigma2.mul(torch.eye(C).to(self.device).expand(N, C, C)).sum(2).view(N, C)
+        aug_result = y + 0.5 * sigma2
+        return aug_result
+
+    def classAug(self, x, y, mix_times=2):
+        batch_size = x.size()[0]
+        mix_data = []
+        mix_target = []
+        for _ in range(mix_times):
+            index = torch.randperm(batch_size).to(self.device)
+            for i in range(batch_size):
+                if y[i] != y[index][i]:
+                    new_label = self.generate_label(y[i].item(), y[index][i].item())
+                    lam = random.uniform(0.4, 0.6)
+                    mix_data.append(lam * x[i] + (1 - lam) * x[index][i])
+                    mix_target.append(new_label)
+        mix_target = torch.Tensor(mix_target).cuda()
+        rot_x = torch.stack([torch.rot90(x, k, (2, 3)) for k in range(1, 4)], 1).view(-1, 3, 32, 32)
+        rot_y = torch.stack([self.numclass * k + y for k in range(1, 4)], 1).view(-1)
+        for item in rot_x:
+            x = torch.cat((x, item.unsqueeze(0)), 0)
+        for item in mix_data:
+            x = torch.cat((x, item.unsqueeze(0)), 0)
+        y = torch.cat((y, rot_y), 0)
+        y = torch.cat((y, mix_target), 0)
+        y = y.long()
+        return x, y
+
+    def generate_label(self, y_a, y_b):
+        if self.old_model is None:
+            y_a, y_b = y_a, y_b
+            assert y_a != y_b
+            if y_a > y_b:
+                tmp = y_a
+                y_a = y_b
+                y_b = tmp
+            label_index = ((2 * self.numclass - y_a - 1) * y_a) / 2 + (y_b - y_a) - 1
+        else:
+            y_a = y_a - (self.numclass - self.task_size)
+            y_b = y_b - (self.numclass - self.task_size)
+            assert y_a != y_b
+            if y_a > y_b:
+                tmp = y_a
+                y_a = y_b
+                y_b = tmp
+            label_index = int(((2 * self.task_size - y_a - 1) * y_a) / 2 + (y_b - y_a) - 1)
+        return label_index + 4 * self.numclass
+
+
+class ActivationsAndGradients:
+    """ Class for extracting activations and
+    registering gradients from targeted intermediate layers """
+
+    def __init__(self, model, target_layers, reshape_transform):
+        self.model = model
+        self.gradients = []
+        self.activations = []
+        self.reshape_transform = reshape_transform
+        self.handles = []
+        for target_layer in target_layers:
+            self.handles.append(
+                target_layer.register_forward_hook(
+                    self.save_activation))
+            if hasattr(target_layer, 'register_full_backward_hook'):
+                self.handles.append(
+                    target_layer.register_full_backward_hook(
+                        self.save_gradient))
+            else:
+                self.handles.append(
+                    target_layer.register_backward_hook(
+                        self.save_gradient))
+
+    def save_activation(self, module, input, output):
+        activation = output
+        if self.reshape_transform is not None:
+            activation = self.reshape_transform(activation)
+        self.activations.append(activation.cpu().detach())
+
+    def save_gradient(self, module, grad_input, grad_output):
+        # Gradients are computed in reverse order
+        grad = grad_output[0]
+        if self.reshape_transform is not None:
+            grad = self.reshape_transform(grad)
+        self.gradients = [grad.cpu().detach()] + self.gradients
+
+    def __call__(self, x):
+        self.gradients = []
+        self.activations = []
+        return self.model(x)
+
+    def release(self):
+        for handle in self.handles:
+            handle.remove()
+
+
+class GradCAM:
+    def __init__(self,
+                 model,
+                 target_layers,
+                 reshape_transform=None,
+                 use_cuda=False):
+        self.model = model.eval()
+        self.target_layers = target_layers
+        self.reshape_transform = reshape_transform
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = model.cuda()
+        self.activations_and_grads = ActivationsAndGradients(
+            self.model, target_layers, reshape_transform)
+
+    """ Get a vector of weights for every channel in the target layer.
+        Methods that return weights channels,
+        
+        will typically need to only implement this function. """
+
+    @staticmethod
+    def get_cam_weights(grads):
+        return np.mean(grads, axis=(2, 3), keepdims=True)
+
+    @staticmethod
+    def get_loss(output, target_category):
+        loss = 0
+        for i in range(len(target_category)):
+            loss = loss + output[i, target_category[i]]
+        return loss
+
+    def get_cam_image(self, activations, grads):
+        weights = self.get_cam_weights(grads)
+        weighted_activations = weights * activations
+        cam = weighted_activations.sum(axis=1)
+
+        return cam
+
+    @staticmethod
+    def get_target_width_height(input_tensor):
+        width, height = input_tensor.size(-1), input_tensor.size(-2)
+        return width, height
+
+    def compute_cam_per_layer(self, input_tensor):
+        activations_list = [a.cpu().data.numpy()
+                            for a in self.activations_and_grads.activations]
+        grads_list = [g.cpu().data.numpy()
+                      for g in self.activations_and_grads.gradients]
+        target_size = self.get_target_width_height(input_tensor)
+        cam_per_target_layer = []
+        # Loop over the saliency image from every layer
+
+        for layer_activations, layer_grads in zip(activations_list, grads_list):
+            cam = self.get_cam_image(layer_activations, layer_grads)
+            cam[cam < 0] = 0  # works like mute the min-max scale in the function of scale_cam_image
+            scaled = self.scale_cam_image(cam, target_size)
+            cam_per_target_layer.append(scaled[:, None, :])
+        return cam_per_target_layer
+
+    def aggregate_multi_layers(self, cam_per_target_layer):
+        cam_per_target_layer = np.concatenate(cam_per_target_layer, axis=1)
+        cam_per_target_layer = np.maximum(cam_per_target_layer, 0)
+        result = np.mean(cam_per_target_layer, axis=1)
+        return self.scale_cam_image(result)
+
+    @staticmethod
+    def scale_cam_image(cam, target_size=None):
+        result = []
+        for img in cam:
+            img = img - np.min(img)
+            img = img / (1e-7 + np.max(img))
+            if target_size is not None:
+                img = cv2.resize(img, target_size)
+            result.append(img)
+        result = np.float32(result)
+
+        return result
+
+    def __call__(self, input_tensor, target_category=None):
+
+        if self.cuda:
+            input_tensor = input_tensor.cuda()
+
+        # 正向传播得到网络输出logits(未经过softmax)
+        output = self.activations_and_grads(input_tensor)
+        if isinstance(target_category, int):
+            target_category = [target_category] * input_tensor.size(0)
+
+        if target_category is None:
+            target_category = np.argmax(output.cpu().data.numpy(), axis=-1)
+        else:
+            assert (len(target_category) == input_tensor.size(0))
+
+        self.model.zero_grad()
+        loss = self.get_loss(output, target_category)
+        loss.backward(retain_graph=True)
+        cam_per_layer = self.compute_cam_per_layer(input_tensor)
+
+        return self.aggregate_multi_layers(cam_per_layer)
+
+    def __del__(self):
+        self.activations_and_grads.release()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.activations_and_grads.release()
+        if isinstance(exc_value, IndexError):
+            # Handle IndexError here...
+            print(
+                f"An exception occurred in CAM with block: {exc_type}. Message: {exc_value}")
+            return True
